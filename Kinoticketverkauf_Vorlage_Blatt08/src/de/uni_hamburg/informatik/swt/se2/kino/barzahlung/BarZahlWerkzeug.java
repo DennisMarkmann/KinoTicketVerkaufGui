d2f032db2f95c6823483008617e3f1d3c9ecfcf6
Dennis.Markmann <Dennis.Markmann@Dennis.local>
2016-06-24 01:54:58 +0200

    Reset to earlier code.

--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/barzahlung/BarZahlWerkzeug.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/barzahlung/BarZahlWerkzeug.java
@@ -1,11 +1,7 @@
 package de.uni_hamburg.informatik.swt.se2.kino.barzahlung;
 
-import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
 
 import de.uni_hamburg.informatik.swt.se2.kino.materialien.Vorstellung;
 
@@ -36,14 +32,6 @@ public class BarZahlWerkzeug
 
     private void fuehreBarZahlungDurch(int betrag)
     {
-        if (istAllesBezahlt())
-        {
-            //TODO eigentlicher Verkauf
-            _barzahlUi.dispose();
-        }
-        _bezahlterBetrag += betrag;
-        _barzahlUi.clearBetragField();
-        _barzahlUi.aktualisiereRestBetrag(berechneDifferenz());
     }
 
     private boolean istAllesBezahlt()
@@ -76,53 +64,5 @@ public class BarZahlWerkzeug
                 }
             });
 
-        _barzahlUi.getBetragField()
-            .getDocument()
-            .addDocumentListener(new DocumentListener()
-            {
-                @Override
-                public void changedUpdate(DocumentEvent e)
-                {
-                    validate();
-                }
-
-                @Override
-                public void insertUpdate(DocumentEvent e)
-                {
-                    validate();
-                }
-
-                private boolean istBetragGueltig()
-                {
-                    if (berechneDifferenz() != 0 && _barzahlUi.getBetragField()
-                        .getText()
-                        .length() == 0 && _barzahlUi.getBetragAsInt() == 0)
-                    {
-                        return false;
-                    }
-                    return true;
-                }
-
-                @Override
-                public void removeUpdate(DocumentEvent e)
-                {
-                    validate();
-                }
-
-                public void validate()
-                {
-                    if (istBetragGueltig())
-                    {
-                        _barzahlUi.changeBetragFieldColor(Color.WHITE);
-                    }
-                    else
-                    {
-                        _barzahlUi.changeBetragFieldColor(Color.RED);
-
-                    }
-                    _barzahlUi.changeOkayButtonState(istBetragGueltig());
-                }
-            });
     }
-
 }
--- Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/barzahlung/BarZahlWerkzeugUI.java
+++ Kinoticketverkauf_Vorlage_Blatt08/src/de/uni_hamburg/informatik/swt/se2/kino/barzahlung/BarZahlWerkzeugUI.java
@@ -1,68 +1,32 @@
 package de.uni_hamburg.informatik.swt.se2.kino.barzahlung;
 
 import java.awt.Color;
-import java.awt.Component;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 
 import javax.swing.JButton;
-import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-class BarZahlWerkzeugUI extends JDialog
+class BarZahlWerkzeugUI extends JFrame
 {
     private static final long serialVersionUID = 4057874294977034472L;
 
-    private JLabel _preisLabelDesc;
     private JLabel _preisLabel;
     private JTextField _betragField;
-    private JLabel _betragLabelDesc;
 
-    private JLabel _restLabelDesc;
     private JLabel _restLabel;
     private JButton _okayButton;
     private JButton _abbrechenButton;
-    private final GridBagConstraints _gridBagConstraints = new GridBagConstraints();
 
     BarZahlWerkzeugUI(int preis)
     {
-        this.setSize(300, 200);
-        this.setModal(true);
-        this.setLocationRelativeTo(null);
-        this.setLayout(new GridBagLayout());
-        _gridBagConstraints.fill = GridBagConstraints.BOTH;
-        _gridBagConstraints.insets = new Insets(5, 5, 5, 5);
-        _gridBagConstraints.weightx = 2;
 
-        _preisLabelDesc = new JLabel("Preis:");
         _preisLabel = new JLabel("" + preis);
-        _betragLabelDesc = new JLabel("Betrag:");
         _betragField = new JTextField();
-        _restLabelDesc = new JLabel("Rest:");
         _restLabel = new JLabel("" + preis);
         _okayButton = new JButton("Okay");
         _okayButton.setEnabled(false);
         _abbrechenButton = new JButton("Abbrechen");
-
-        addComponent(_preisLabelDesc, 0, 0);
-        addComponent(_preisLabel, 1, 0);
-        addComponent(_betragLabelDesc, 0, 1);
-        addComponent(_betragField, 1, 1);
-        addComponent(_restLabelDesc, 0, 2);
-        addComponent(_restLabel, 1, 2);
-        addComponent(_okayButton, 0, 3);
-        addComponent(_abbrechenButton, 1, 3);
-
-    }
-
-    private void addComponent(final Component component, final int gridxValue,
-            final int gridyValue)
-    {
-        _gridBagConstraints.gridx = gridxValue;
-        _gridBagConstraints.gridy = gridyValue;
-        this.add(component, _gridBagConstraints);
     }
 
     void aktualisiereRestBetrag(int restBetrag)
@@ -90,18 +54,10 @@ class BarZahlWerkzeugUI extends JDialog
         return _abbrechenButton;
     }
 
-    int getBetragAsInt()
+    public int getBetragAsInt()
     {
-        int betrag = 0;
-        try
-        {
-            betrag = Integer.parseInt(getBetragText());
-            return betrag;
-        }
-        catch (NumberFormatException e)
-        {
-            return betrag;
-        }
+        // TODO Auto-generated method stub
+        return 0;
     }
 
     JTextField getBetragField()
